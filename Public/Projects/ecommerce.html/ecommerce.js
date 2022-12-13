c8d0f60220959b591f0d0f35182ab87be752ff1e
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2022-12-13 22:07:42 +0530

    project

---
+++ Public/Projects/ecommerce.html/ecommerce.js
@@ -0,0 +1,14 @@
+const close = document.getElementById('close')
+const nav = document.getElementById('navbar')
+
+if (bar) {
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active')
+    })
+}
+
+if (close) {
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active')
+    })
+}
