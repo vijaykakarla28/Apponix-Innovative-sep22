0421815ea1fed51fa4c4f3ce2bb3040d7e18b6c8
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2022-12-19 11:50:55 +0530

    express intro

---
+++ Fullstack/Backend/Frameworks/app.ts
@@ -0,0 +1,31 @@
+var express=require('express')
+var app=express()
+
+app.get('/flight',(req:any,res:any)=>{
+    res.send('enjoy the party')
+})
+
+app.post('/user',(req:any,res:any)=>{
+    res.send('Apponix Student post')
+})
+
+app.delete('/user',(req:any,res:any)=>{
+    res.send('Apponix Student delete')
+})
+
+app.put('/user',(req:any,res:any)=>{
+    res.send('Apponix Student put or update')
+})
+
+app.get('/user',(req:any,res:any)=>{
+    res.send('Apponix Student get')
+})
+
+app.get('/trailer',(req:any,res:any)=>{
+    var trailer='<iframe width="560" height="315" src="https://www.youtube.com/embed/d9MyW72ELq0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    res.send(trailer)
+})
+
+app.listen(8003,()=>{
+    console.log('server started on port no 8003')
+})
