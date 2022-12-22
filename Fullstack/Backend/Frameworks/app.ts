cc5328b75e190e6368c680ff635ce26344d2348d
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2022-12-22 15:38:58 +0530

    static path img

--- Fullstack/Backend/Frameworks/app.ts
+++ Fullstack/Backend/Frameworks/app.ts
@@ -2,7 +2,7 @@ var express=require('express')
 var app=express()
 var cors=require('cors')
 var bodyParser=require('body-parser')
-
+var path=require('path')
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:false}))
 
@@ -10,25 +10,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/cars',(req:any,res:any)=>{
-    console.log(req.query)
-    console.log(req.body)
-
-    //console.log(req.body)
-    res.send('Welcome to this website')
-})
-
-app.post('/cars',(req:any,res:any)=>{
-    console.log(req.query.username)
-    res.send(req.query.username)
-})
-
-app.get('/user/:id',(req:any,res:any)=>{
-    console.log(req.params.id)
-    res.send(req.params.id)
-})
-
-
+app.use(express.static(path.join(__dirname,'src/public/img')))
 
 app.listen(8003,()=>{
     console.log('server started on port no 8003')
