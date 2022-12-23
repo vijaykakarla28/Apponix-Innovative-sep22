d37b360165bd065338672004499b563e170150e0
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2022-12-23 21:56:49 +0530

    ejs videos app.route

--- Fullstack/Backend/Frameworks/app.ts
+++ Fullstack/Backend/Frameworks/app.ts
@@ -3,6 +3,9 @@ var app=express()
 var cors=require('cors')
 var bodyParser=require('body-parser')
 var path=require('path')
+var ejs=require('ejs')
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'src/views'))
 
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:false}))
@@ -12,8 +15,30 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.use(express.static(path.join(__dirname,'src/public/')))
+app.use(express.static(path.join(__dirname,'src/videos')))
+
+
+
+app.route('/')
+.get((req:any,res:any)=>{
+    res.render('home')
+})
+.post((req:any,res:any)=>{
+    res.send('Hello')
+})
+.delete((req:any,res:any)=>{
+    res.send('Hello')
+})
+.put((req:any,res:any)=>{
+    res.send('Hello')
+})
+
 
 
 app.listen(8003,()=>{
     console.log('server started on port no 8003')
 })
+
+function route(arg0: string) {
+    throw new Error("Function not implemented.")
+}
