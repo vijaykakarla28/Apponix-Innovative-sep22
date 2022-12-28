6539e467bc77183f5c8c5a7b7670013f99e3b1e7
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2022-12-28 18:46:58 +0530

    register api is done

--- Fullstack/Backend/Frameworks/src/controller/index.ts
+++ Fullstack/Backend/Frameworks/src/controller/index.ts
@@ -1,1 +1,2 @@
 export * from './home/home.Ctrl'
+export * from './auth'
---
+++ Fullstack/Backend/Frameworks/src/routes/auth/registration/userReg.routes.ts
@@ -0,0 +1,6 @@
+import { Router } from "../../../../config/common.config";
+import { userReg_Post } from "../../../controller";
+
+Router.route('./register')
+.post(userReg_Post)
+module.exports=Router
