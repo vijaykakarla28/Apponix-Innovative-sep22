73164f117b0d766a70ae090b647df0c654101fad
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2023-01-02 12:45:42 +0530

    project

--- Public/Projects/Portfolio/script.js
+++ Public/Projects/Portfolio/script.js
@@ -10,4 +10,20 @@ let navlist = document.querySelector('.navlist');
 menu.onClick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('active');
-}
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+    distance : '45px',
+    duration :2700,
+    reset : true
+})
+
+sr.reveal('.home-text',{delay:350 , origin:'left'})
+sr.reveal('.home-img',{delay:350 , origin:'right'})
+
+sr.reveal('.sub-service','.about','portfolio','.service','.cta','.contact',{delay:200 , origin:'bottom'})
