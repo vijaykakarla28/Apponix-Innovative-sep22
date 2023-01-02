343a8a7e5c60235bb40cd4bb39ec4f9224e68cd1
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2023-01-02 12:23:15 +0530

    project

--- Public/Projects/Portfolio/script.js
+++ Public/Projects/Portfolio/script.js
@@ -3,3 +3,11 @@ const header = document.querySelector("header");
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",window.scrollY>0);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onClick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+}
