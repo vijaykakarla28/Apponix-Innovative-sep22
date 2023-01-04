0f5564e17c6d093f48b151b514077296dd40cb01
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2023-01-04 19:57:28 +0530

    worked on seller upload api

---
+++ Fullstack/Backend/Frameworks/src/controller/seller/sellerUpload.Ctrl.ts
@@ -0,0 +1,7 @@
+export const sellerUpload_Post=async(req:any,res:any)=>{
+    let image=req.body.image
+    let discription=req.body.discription
+    let title=req.body.title
+    let price=req.body.price
+    console.log(image.discription.title.price)
+}
--- Fullstack/Backend/Frameworks/src/routes/index.ts
+++ Fullstack/Backend/Frameworks/src/routes/index.ts
@@ -1,5 +1,6 @@
 const homeRoute=require('./home/home.routes')
 const register=require('./auth/registration/userReg.routes')
 const userlogin=require('./auth/login/login.routes')
+const sellerUploadProduct=require('./seller/sellerUpload.routes')
 
-module.exports={homeRoute,register,userlogin}
+module.exports={homeRoute,register,userlogin,sellerUploadProduct}
