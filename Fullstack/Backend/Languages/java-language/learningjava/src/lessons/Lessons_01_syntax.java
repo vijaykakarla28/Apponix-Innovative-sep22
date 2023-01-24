ec8ca7f88e85f9057342a589f026facb6615fcb1
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2023-01-24 10:56:04 +0530

    java syntax

---
+++ Fullstack/Backend/Languages/java-language/learningjava/src/lessons/Lessons_01_syntax.java
@@ -0,0 +1,19 @@
+package lessons;
+
+public class Lessons_01_syntax {
+String car="i am car";
+int money=50;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("If creating class make sure 1st letter should be capital");
+		 System.out.println("the class has {}--body class");
+		 System.out.println("the class with in the body properties methods");
+		 System.out.println("when ur creating properties make sure you tell first only type of it");
+		 System.out.println("when creating the variables make sure you define");
+		 
+	}
+
+}
+class Human{
+	
+}
