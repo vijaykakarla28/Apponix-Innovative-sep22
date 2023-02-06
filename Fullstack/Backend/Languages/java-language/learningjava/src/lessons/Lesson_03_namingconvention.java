e8cbdec32d875e7633108b09de19d467e330c708
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2023-02-06 10:45:55 +0530

    java namingconventions and comments

--- Fullstack/Backend/Languages/java-language/learningjava/src/lessons/Lesson_03_namingconvention.java
+++ Fullstack/Backend/Languages/java-language/learningjava/src/lessons/Lesson_03_namingconvention.java
@@ -4,16 +4,16 @@ public class Lesson_03_namingconvention {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//int -money=10;
-//int money=10;
-//int !money=10;
-//int )money=10;
-//int +money=10;
-//int 1money=10;
+/*int -money=10;
+int money=10;
+int !money=10;
+int )money=10;
+int +money=10;
+int 1money=10;*/
 int m1oney=10;
 int money11=10;
 int mo_ney=10;
-
+System.out.println(m1oney+money11+mo_ney);
 	}
 
 }
--- Fullstack/Backend/Languages/java-language/learningjava/src/lessons/Lesson_04_comments.java
+++ Fullstack/Backend/Languages/java-language/learningjava/src/lessons/Lesson_04_comments.java
@@ -5,8 +5,8 @@ public class Lesson_04_comments {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //single line comment --shortcut ctrl+/
-		int a;
-		int b;
+//		int a;
+//		int b;
 //		int a=10;
 //		this is called multi line comments
 	/*	int a;
