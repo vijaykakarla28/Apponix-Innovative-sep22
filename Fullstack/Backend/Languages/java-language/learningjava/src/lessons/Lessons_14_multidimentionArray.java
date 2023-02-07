bd4dd6dd29161d20bf49c12898e9d6bb8917e900
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2023-02-07 15:20:03 +0530

    methods multidimentions

---
+++ Fullstack/Backend/Languages/java-language/learningjava/src/lessons/Lessons_14_multidimentionArray.java
@@ -0,0 +1,16 @@
+package lessons;
+
+public class Lessons_14_multidimentionArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] m= {1,2,3,4};
+		int [] m1= {1,2,3,4,5};
+		int [][] multi= {{1,2,3,4},{1,2,3,4,5}};
+		System.out.println(multi[0][1]);
+		
+		
+	}
+
+}
