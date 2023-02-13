b78ee9a028789e8472de1e66c6d42475667679b2
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2023-02-13 22:33:08 +0530

    errors files

---
+++ Fullstack/Backend/Languages/java-language/learningjava/src/advance_java/Lessons_09_Files.java
@@ -0,0 +1,13 @@
+package advance_java;
+
+import java.io.File;
+
+public class Lessons_09_Files {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+File m=new File("/file.txt");
+System.out.println("file created");
+	}
+
+}
