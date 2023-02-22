5122aa7adbbf75409310e88e8f13bc71297ad5ea
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2023-02-22 18:50:56 +0530

    Spring Boot

---
+++ Fullstack/Backend/Springboot-java/serversite-rendering/src/main/java/com/example/demo/Mycontroller.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Mycontroller {
+
+	@RequestMapping(value="/home",method=RequestMethod.GET)
+	@ResponseBody
+	public ModelAndView handel() {
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("home");
+	return mv;
+	}
+}
