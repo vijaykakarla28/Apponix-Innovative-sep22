91ff79d063e928ce6dd584db42e3ec3dbc5f33a0
vijaykakarla28 <117249984+vijaykakarla28@users.noreply.github.com>
2023-02-27 11:27:02 +0530

    Controller

--- Fullstack/Backend/Springboot-java/serversite-rendering/src/main/java/com/example/demo/Mycontroller.java
+++ Fullstack/Backend/Springboot-java/serversite-rendering/src/main/java/com/example/demo/Mycontroller.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,10 +11,23 @@ import org.springframework.web.servlet.ModelAndView;
 public class Mycontroller {
 
 	@RequestMapping(value="/home",method=RequestMethod.GET)
-	@ResponseBody
+	//@ResponseBody
 	public ModelAndView handel() {
 		ModelAndView mv=new ModelAndView();
 		mv.setViewName("home");
 	return mv;
 	}
+	
+	@RequestMapping(value="/about",method=RequestMethod.GET)
+	//@ResponseBody
+	public ModelAndView handel2(ModelMap model) {
+		ModelAndView mv=new ModelAndView();
+	
+		model.put("content", "Hero");
+		//System.out.println(mv.addObject("user", p));
+		mv.setViewName("about");
+	return mv;
+	}
 }
+
+
